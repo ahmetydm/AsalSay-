ac7a5bbb6f77f50dfadf3534f79f839ecb782513
ahmetydm <ahmet.k.ydm@gmail.com>
2024-10-11 13:19:31 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,17 +4,30 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        int asal = scan.nextInt();
-        int sum = 0;
-        for (int i=2; i<asal; i++){
-            if (asal%i==0){
-                sum++;
-            }
+        System.out.println("ilk sayı");
+        double sayı1 = scan.nextDouble();
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("ikinci sayı");
+        double sayı2 = scan2.nextDouble();
+        Scanner scan3 = new Scanner(System.in);
+        System.out.println("Yapmak istediğiniz işlemi seçiniz");
+        System.out.println("1-toplama \n2-çıkartma \n3-çarpma \n4-bölme ");
+        int işlem = scan3.nextInt();
+
+
+       if (işlem==1){
+            System.out.println(sayı1+sayı2);
         }
-        if (sum==0){
-            System.out.println(asal+" Asaldır");
-        }else{
-            System.out.println(asal+" Asal değildir");
+        else if (işlem==2){
+            System.out.println(sayı1-sayı2);
         }
+        else if (işlem==3){
+            System.out.println(sayı1*sayı2);
+        }
+        else if (işlem==4){
+            System.out.println(sayı1/sayı2);
+        }else {
+           System.out.println("Geçersiz işlem");
+       }
     }
 }
