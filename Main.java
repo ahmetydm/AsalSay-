dcf4e41fa62960010cec9c2560916616c6449feb
ahmetydm <ahmet.k.ydm@gmail.com>
2024-10-11 13:36:39 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        int asal = scan.nextInt();
+        int prime = scan.nextInt();
         int sum = 0;
-        for (int i=2; i<asal; i++){
-            if (asal%i==0){
+        for (int i=2; i<prime; i++){
+            if (prime%i==0){
                 sum++;
             }
         }
         if (sum==0){
-            System.out.println(asal+" Asaldır");
+            System.out.println(prime+" Asaldır");
         }else{
-            System.out.println(asal+" Asal değildir");
+            System.out.println(prime+" Asal değildir");
         }
     }
 }
